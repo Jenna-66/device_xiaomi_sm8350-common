b85d76f93536148242c99142cb6dc48a70adede7
Jenna <jenna@jennalody.de>
2025-08-13 18:52:00 +0200

    sm8350-common: remove some packages I don't want
    Signed-off-by: Jenna <jenna@jennalody.de>

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,7 +4,23 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := YouTube 
+LOCAL_OVERRIDES_PACKAGES := \
+    com.android.hotwordenrollment.common.util \
+    Browser2 \
+    Calendar \
+    Etar \
+    ExactCalculator \
+    Gallery2 \
+    Glimpse \
+    HotwordEnrollmentOKGoogleHEXAGON_WIDEBAND \
+    HotwordEnrollmentXGoogleHEXAGON_WIDEBAND \
+    Jelly \
+    MatLog \
+    Music \
+    MusicFX \
+    SystemUpdater \
+    Twelve \
+    Updater
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
