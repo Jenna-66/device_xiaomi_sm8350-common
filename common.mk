97ff81c0ffd80447d3ab3fabac80974cb8531b2b
0mar99 <omarag9099@gmail.com>
2025-12-15 15:52:30 -0600

    sm8350-common: Build GameBar
    Signed-off-by: 0mar99 <omarag9099@gmail.com>

--- common.mk
+++ common.mk
@@ -23,6 +23,9 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 # Add common definitions for Qualcomm
 $(call inherit-product, hardware/qcom-caf/common/common.mk)
 
+# GameBar Performance Overlay
+$(call inherit-product-if-exists, packages/apps/GameBar/gamebar.mk)
+
 # API
 PRODUCT_SHIPPING_API_LEVEL := 30
 
