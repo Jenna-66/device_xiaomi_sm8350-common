b8284564624dcb7d79852b35ba17e6ca6fc51b04
Jenna-66 <jenna@jennalody.de>
2026-03-18 22:00:54 +0100

    sm8350-common: Removed google-shit from vendor
    So no need to block it here.
    Also don't add privapp-permissions for it.
    
    Signed-off-by: Jenna-66 <jenna@jennalody.de>

--- common.mk
+++ common.mk
@@ -242,10 +242,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.health-service.qti
 
-# HotwordEnrollement
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-hotword.xml
-
 # Init scripts
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/etc/init.batterysecret.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.batterysecret.rc \
