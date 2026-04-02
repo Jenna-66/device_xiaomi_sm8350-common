6c23a6a1713c2dda6623fabcf8e77ac721a521b2
0mar99 <omarag9099@gmail.com>
2026-04-01 23:05:46 -0600

    sm8350-common: Build TouchServices
    Signed-off-by: 0mar99 <omarag9099@gmail.com>

--- common.mk
+++ common.mk
@@ -476,6 +476,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml
 
+# TouchServices
+$(call inherit-product, packages/apps/TouchServices/touchservice.mk)
+
 # Ueventd
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/etc/ueventd.rc:$(TARGET_COPY_OUT_VENDOR)/etc/ueventd.rc
