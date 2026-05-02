b58ae031a729a98b3b935d5d7f850f0f6abdd4cd
Jenna-66 <jenna@jennalody.de>
2026-05-02 22:14:12 +0200

    sm8350-common: Allow to chose between sony and default dolby

--- common.mk
+++ common.mk
@@ -167,7 +167,8 @@ PRODUCT_COPY_FILES += \
     hardware/qcom-caf/sm8350/display/config/snapdragon_color_libs_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/snapdragon_color_libs_config.xml
 
 # Dolby
-$(call inherit-product, vendor/sony/dolby/dolby.mk)
+$(call inherit-product-if-exists, vendor/sony/dolby/dolby.mk)
+$(call inherit-product-if-exists, hardware/dolby/dolby.mk)
 
 # DRM
 PRODUCT_PACKAGES += \
